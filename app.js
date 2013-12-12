4c2dc4056bb6627eb4ee323a1b871a0196a7ee5a
David Taylor <david.taylor@emberex.com>
2013-12-12 09:44:13 -0800

    Second dojo session setup.

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
 var foo = angular.module('dojoApp', []);
 
 foo.controller("dojoController", function($scope, SaveService) {
-
-	$scope.name = "Hello!";
-
-	$scope.validateName=function(){
-		if($scope.name == "casey"){
-			alert('valid!');
+	$scope.submitForm = function() {
+		if ($scope.userform.$valid) {
+			SaveService.saveUser($scope.user);
+			$scope.user = {};
+			$scope.userform.$setPristine();
+		} else {
+			alert("Fix your invalid input, dude.");
 		}
 	};
-	
-	$scope.saveStuff = function(thing) {
-		SaveService.saveStuff(thing);
-	}
-	
+
+	$scope.getPeople = function() {
+		return SaveService.getAllUsers();
+	};
 });
 
 foo.factory('SaveService', function() {
-	var datas = [];
-	
+	var data = {};
 	return {
-		saveStuff: function(thing) {
-			datas.push(thing);
-			console.log('datas: ', datas);
+		saveUser: function(user) {
+			data[user.name] = user;
+		},
+		getUser: function(name) {
+			return data[name];
+		},
+		getAllUsers: function() {
+			return data;
+		}
+	};
+});
+
+foo.directive("notMichael", function() {
+	return {
+		require: "ngModel",
+		link: function(scope, element, attrs, controller) {
+			scope.$watch(attrs.ngModel, function() {
+				if (scope.user && scope.user.name && scope.user.name.match(/[Mm]i(ke|chael)/)) {
+					controller.$setValidity('notMichael', false);
+				} else {
+					controller.$setValidity('notMichael', true);
+				}
+			});
 		}
 	}
+});
 
+foo.directive("dojoListItem", function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		template: "<li>{{person}}</li>",
+		link: function(scope, element) {
+		}
+	};
 });
