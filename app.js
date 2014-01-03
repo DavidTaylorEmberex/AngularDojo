67e41d93f288adaeab11ad312e16121b24d7ee48
David Taylor <david.taylor@emberex.com>
2014-01-02 16:18:08 -0800

    Third dojo session setup.

--- app.js
+++ app.js
@@ -1,65 +1,102 @@
-var foo = angular.module('dojoApp', []);
+(function() {
+    if (!String.format) {
+        String.format = function (format) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            var sprintfRegex = /\{(\d+)\}/g;
+            var sprintf = function (match, number) {
+                return number in args ? args[number] : match;
+            };
+            return format.replace(sprintfRegex, sprintf);
+        };
+    }
 
-foo.controller("dojoController", function($scope, SaveService) {
-	$scope.submitForm = function() {
-		if ($scope.userform.$valid) {
-			SaveService.saveUser($scope.user);
-			$scope.user = {};
-			$scope.userform.$setPristine();
-			$scope.people = getPeople();
-		} else {
-			alert("Fix your invalid input, dude.");
-		}
-	};
+    var foo = angular.module('dojoApp', []);
 
-	getPeople = function() {
-		console.log("getting people");
-		return SaveService.getAllUsers();
-	};
-});
+    foo.controller("dojoController", function($scope, SaveService) {
+        function reset() {
+            $scope.user = {
+                name: "",
+                email: "",
+                gender: ""
+            };
+            if ($scope.userform) {
+                $scope.userform.$setPristine();
+            }
+            $scope.people = getPeople();
+        }
+        
+        $scope.submitForm = function() {
+            if ($scope.userform.$valid) {
+                SaveService.saveUser($scope.user);
+                reset();
+            } else {
+                alert("Fix your missing input, dude.");
+            }
+        };
+        
+        $scope.killPeople = function() {
+            SaveService.clearUsers();
+            reset();
+        };
 
-foo.factory('SaveService', function() {
-	var data = {};
-	return {
-		saveUser: function(user) {
-			data[user.name] = user;
-		},
-		getUser: function(name) {
-			return data[name];
-		},
-		getAllUsers: function() {
-			return data;
-		}
-	};
-});
+        function getPeople() {
+            return SaveService.getAllUsers();
+        }
+        
+        reset();
+    });
 
-foo.directive("testMe", function() {
-	return {
-		restrict: 'CMEA',
-		replace: true,
-		template: "<b>MEEEEEEE!<b>"
-	}
-});
+    foo.factory('SaveService', function() {
+        var data = {};
+        return {
+            saveUser: function(user) {
+                data[user.name] = user;
+            },
+            getUser: function(name) {
+                return data[name];
+            },
+            getAllUsers: function() {
+                return data;
+            },
+            clearUsers: function() {
+                data = {};
+            }
+        };
+    });
 
-foo.directive("notMichael", function() {
-	return {
-		require: "ngModel",
-		link: function(scope, element, attrs, controller) {
-			scope.$watch(attrs.ngModel, function() {
-				if (scope.user && scope.user.name && scope.user.name.match(attrs.notMe)) {
-					controller.$setValidity('notMichael', false);
-				} else {
-					controller.$setValidity('notMichael', true);
-				}
-			});
-		}
-	}
-});
-
-foo.directive("dojoListItem", function() {
-	return {
-		restrict: 'E',
-		replace: true,
-		template: "<li><pre>{{person}}</pre></li>"
-	};
-});
+    foo.directive("genderOptions", ["$compile", function($compile) {
+        var genders = ["Male", "Female"];
+        return {
+            restrict: "E",
+            templateUrl: "template.html",
+            replace: true,
+            controller: "dojoController",
+            compile: function(element, attributes) {
+                var optionString = '<div ng-repeat="gender in foo"><input type="radio" id="{{gender}}" value="{{gender}}" name="gender" ng-model="user.gender"/><label for="{{gender}}">{{gender}}</label><br/></div>';
+                var radioOption = angular.element(optionString);
+                element.prepend(radioOption);
+                
+                return {
+                    pre: function(scopePre, elementPre, attrsPre) {
+                        scopePre.foo = genders;
+                        scopePre.$watch(function() {
+                            return scopePre.user.name;
+                        }, function() {
+                            scopePre.name = scopePre.user.name;
+                        });
+                        genders.push("Other");
+                    },
+                    
+                    post: function(scopePost, elementPost, attrsPost) {
+                        scopePost.$watch(function() {
+                            return scopePost.user.gender;
+                        }, function() {
+                            scopePost.gender = scopePost.user.gender;
+                        });
+                        genders.push("Undisclosed");
+                    }
+                };
+            }
+        };
+    }]);
+}());
